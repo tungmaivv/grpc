9c97c1da672fe477680a5b76e55fa40f756c6cad
Tung Vu Mai <tungmaiv@hotmail.com>
2021-04-27 17:20:01 +0700

    update

--- online_book_store/recommendations/recommendations.py
+++ online_book_store/recommendations/recommendations.py
@@ -50,15 +50,24 @@ class RecommendationService(
 
         return RecommendationResponse(recommendations=books_to_recommend)
     
-    def serve():
-        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        recommendations_pb2_grpc.add_RecommendationsServicer_to_server(
-            RecommendationService(), server
-        )
-        server.add_insecure_port("[::]:50051")
-        server.start()
-        server.wait_for_termination()
+def serve():
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    recommendations_pb2_grpc.add_RecommendationsServicer_to_server(
+
+        RecommendationService(), server
+
+    )
+
+    server.add_insecure_port("[::]:50051")
+
+    server.start()
+
+    server.wait_for_termination()
+
 
 
 if __name__ == "__main__":
-    serve()  
+
+    serve()
